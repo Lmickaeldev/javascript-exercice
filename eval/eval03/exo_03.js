8cf6b5037fa967b7e71eb147dd32837d56bc4c1b
lmickael <mickael60000lachevre@gmail.com>
2023-01-24 18:42:32 +0100

    encore une fois

--- eval/eval03/exo_03.js
+++ eval/eval03/exo_03.js
@@ -27,14 +27,48 @@
 //          break;
 //     }
 // }
- let table = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
-let Prenom = prompt("devinez le prenom")
-let index = table.indexOf(Prenom)
-if (index!== -1){
-    let i=index;i <table.length -1 ;i++
-    table[i]=table[i+1]
-    table[table.length-1]="";
-    document.write("bravo!!"+Prenom+" a été trouvé "+"<br>")
-}
-else
-alert("ERREUR "+Prenom+" n'existe pas dans le tableau.")
+
+//  let table = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+//  console.table(table)
+// let Prenom = prompt("devinez le prenom")
+// let index = table.indexOf(Prenom)
+// if (index!== -1){
+//     let i=index;i <table.length-1 ;i++
+//     table[i]=table[i+1]
+//     table[table.length-1]="";
+//     document.write("bravo!!"+Prenom+" a été trouvé "+"<br>")
+//     alert(table)
+    
+// }
+// else
+// alert("ERREUR "+Prenom+" n'existe pas dans le tableau.")
+
+// tab[i] devien tab[-1]
+
+let table = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+
+
+let trouve = true;
+
+while (trouve==true){
+let Prenom=prompt("devinez le prénom")
+let index=table.indexOf(Prenom)
+    if (index !== -1){
+        for (i=index ;i <table.length -1;i++)
+        {
+            table[i]=table[i+1]  
+            table[table.length-1]="";
+        
+        }   
+        
+            document.write("bravo!!"+Prenom+" a été trouvé "+"<br>")
+            trouve=false
+    }
+            
+    else 
+    {
+         alert("ERREUR ce "+Prenom+" n'existe pas dans le tableau."+(!trouve))
+         
+         break;
+    }
+}alert(table)
