8a6e2d518adb269177ded57f7b041dad02611a89
lmickael <mickael60000lachevre@gmail.com>
2023-01-24 17:01:41 +0100

    mise a jour sur demande

--- eval/eval03/exo_03.js
+++ eval/eval03/exo_03.js
@@ -1,33 +1,40 @@
 
 
-let table = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+// let table = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
 
 
-let trouve = true;
+// let trouve = true;
 
-while (trouve==true){
-let Prenom=prompt("devinez le prénom")
-let index=table.indexOf(Prenom)
-    if (index !== -1){
-        for (i=index ;i <table.length -1;i++)
-        {
-            table[i]=table[i+1]  
-            table[table.length-1]="";
+// while (trouve==true){
+// let Prenom=prompt("devinez le prénom")
+// let index=table.indexOf(Prenom)
+//     if (index !== -1){
+//         for (i=index ;i <table.length -1;i++)
+//         {
+//             table[i]=table[i+1]  
+//             table[table.length-1]="";
         
-        }   
+//         }   
         
-            document.write("bravo!!"+Prenom+" a été trouvé "+"<br>")
+//             document.write("bravo!!"+Prenom+" a été trouvé "+"<br>")
             
-    }
+//     }
             
-    else 
-    {
-         alert("ERREUR ce "+Prenom+" n'existe pas dans le tableau."+(!trouve))
+//     else 
+//     {
+//          alert("ERREUR ce "+Prenom+" n'existe pas dans le tableau."+(!trouve))
          
-         break;
-    }
+//          break;
+//     }
+// }
+ let table = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+let Prenom = prompt("devinez le prenom")
+let index = table.indexOf(Prenom)
+if (index!== -1){
+    let i=index;i <table.length -1 ;i++
+    table[i]=table[i+1]
+    table[table.length-1]="";
+    document.write("bravo!!"+Prenom+" a été trouvé "+"<br>")
 }
-
-
-
-
+else
+alert("ERREUR "+Prenom+" n'existe pas dans le tableau.")
